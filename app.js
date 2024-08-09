33cab413e90ad32e3285e44c305e4e8536586b84
Karsondeveloper <164472789+Karsondeveloper@users.noreply.github.com>
2024-08-09 19:11:14 +0200

    more, again update

---
+++ app.js
@@ -0,0 +1,45 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carroselDom = document.getElementById('.carrosel');
+let listItemDom = document.getElementById('.carrosel .list');
+let thumbnailDom = document.getElementById('.carrosel .thumbnail');
+
+nextDom.onclick = function(){
+    showSlider('next');
+}
+prevDom.onclick = function(){
+    showSlider('prev');
+}
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+let runTimeDut;
+let runAutoRun  = setTimeout(()=>{
+    nextDom.click();
+}, timeAutoNext);;
+
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carrosel .list .item')
+    let itemThumbnail = document.querySelectorAll('.carrosel .thumbnail .item')
+
+    if(type === 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carroselDom.classList.add('next');
+    }else{
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        carroselDom.classList.add('prev');
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() =>{
+        carroselDom.classList.remove('next');
+        carroselDom.classList.remove('prev');
+    }, timeRunning)
+
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(()=>{
+        nextDom.click();
+    }, timeAutoNext);
+}
